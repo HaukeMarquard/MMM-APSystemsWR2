d758e9929e50c7cfb920d2fcbe35dcc348c29628
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-12-11 14:31:52 +0100

    .

--- MMM-APSystemsWR2.js
+++ MMM-APSystemsWR2.js
@@ -57,14 +57,14 @@ Module.register("MMM-APSystemsWR2", {
       value_one_container.classList.add("value_one_container");
       value_one_container.classList.add("value_container");
       const value_one = document.createElement("p");
-      value_one.innerText = `${this.weather.data.p1}`;
+      value_one.innerText = `${this.weather.data.p2}`;
       value_one_container.appendChild(value_one);
 
       const value_two_container = document.createElement("div");
       value_two_container.classList.add("value_two_container");
       value_two_container.classList.add("value_container");
       const value_two = document.createElement("p");
-      value_two.innerText = `${this.weather.data.p2}`;
+      value_two.innerText = `${this.weather.data.p1}`;
       value_two_container.appendChild(value_two);
 
       const output_value = this.weather.data.p1 + this.weather.data.p2;
@@ -86,22 +86,9 @@ Module.register("MMM-APSystemsWR2", {
       const daily_value = document.createElement("p");
       daily_value.innerText = `Tagesertrag: ${this.daily_value.toFixed(
         2
-      )} / Gesamterzeugung: ${(
+      )} kW / Gesamterzeugung: ${(
         this.weather.data.te1 + this.weather.data.te2
-      ).toFixed(2)}`;
-
-      //   const rain_container = document.createElement("div");
-      //   rain_container.style.display = "flex";
-      //   rain_container.style.align_items = "center";
-      //   rain_container.style.justifyContent = "center";
-      //   const rain = document.createElement("p");
-      //   rain.innerText = `20%`;
-      //   const r_img = document.createElement("img");
-      //   r_img.classList.add("small");
-      //   r_img.src = "/MMM-APSystemsWR2/solaranlage.png";
-      //   rain_container.appendChild(r_img);
-      //   rain_container.appendChild(rain);
-      //   container.appendChild(rain_container);
+      ).toFixed(2)} kW`;
 
       container.appendChild(img);
       container.appendChild(value_one_container);
@@ -109,41 +96,6 @@ Module.register("MMM-APSystemsWR2", {
       container.appendChild(wr_container);
       container.appendChild(output_container);
       container.appendChild(daily_value);
-      //   container.classList.add("container");
-      //   var output = document.createElement("div");
-      //   output.classList.add("output");
-      //   const output_value = this.weather.data.p1 + this.weather.data.p2;
-      //   output.innerText = `${output_value > 600 ? 600 : output_value} W`;
-      //   const wr = document.createElement("div");
-      //   wr.classList.add("wr");
-      //   const wr_value = document.createElement("p");
-      //   wr_value.innerText = `${output_value} W`;
-      //   wr.appendChild(wr_value);
-      //   const strang_one = document.createElement("div");
-      //   strang_one.classList.add("strang_one_line");
-      //   const strang_one_line = document.createElement("p");
-      //   strang_one_line.innerText = `---------`;
-      //   strang_one.appendChild(strang_one_line);
-      //   const strang_one_value = document.createElement("p");
-      //   strang_one_value.innerText = `${this.weather.data.p1} W`;
-      //   strang_one.appendChild(strang_one_value);
-      //   const strang_two = document.createElement("div");
-      //   strang_two.classList.add("strang_two_line");
-      //   const strang_two_line = document.createElement("p");
-      //   strang_two_line.innerText = `---------`;
-      //   strang_two.appendChild(strang_two_line);
-      //   const strang_two_value = document.createElement("p");
-      //   strang_two_value.innerText = `${this.weather.data.p2} W`;
-      //   strang_two.appendChild(strang_two_value);
-
-      //   const test_p = document.createElement("p");
-      //   test_p.innerText = `123`;
-      //   test_p.classList.add("test_p");
-      //   container.appendChild(output);
-      //   container.appendChild(wr);
-      //   container.appendChild(strang_one);
-      //   container.appendChild(strang_two);
-      //   container.appendChild(test_p);
       return container;
     }
   },
